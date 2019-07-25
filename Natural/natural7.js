b17202b158979147b2a3a8fe3e7aaf12dee0a203
nicolas-suarez-97 <nicolas.suarez10@hotmail.com>
2019-07-25 12:27:01 -0500

    Nuevas Modificaciones

---
+++ Natural/natural7.js
@@ -0,0 +1,30 @@
+var authNo = document.getElementById("autNo");
+var authSi = document.getElementById("autSi");
+var lectNo = document.getElementById("lecturaNo");
+var lectSi = document.getElementById("lecturaSi");
+
+var button = document.getElementById("submitButton");
+
+authNo.addEventListener("click",function(){        
+    button.disabled = validateButton();
+});
+
+authSi.addEventListener("click",function(){        
+    button.disabled = validateButton();    
+});
+
+lectNo.addEventListener("click",function(){        
+    button.disabled = validateButton();    
+});
+
+lectSi.addEventListener("click",function(){        
+    button.disabled = validateButton();    
+});
+
+function validateButton(){
+    if(authSi.checked & lectSi.checked){
+        return false;
+    }else{
+        return true;
+    }
+}
