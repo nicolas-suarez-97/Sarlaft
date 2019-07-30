43b01a821b58308f965274179b42239c693827e0
nicolas-suarez-97 <nicolas.suarez10@hotmail.com>
2019-07-30 11:54:03 -0500

    Arreglos menores

--- Juridico/juridico9.js
+++ Juridico/juridico9.js
@@ -1,23 +1,4 @@
-var form = document.getElementById("form-id");
-var option = document.getElementById('option');
 
-document.getElementById("add").addEventListener("click", function () {
-    console.log("add");
-    option.value="add";
-    console.log(option);
-    form.submit();
-  
-});
-
-document.getElementById("delete").addEventListener("click", function () {
-    //form.submit();
-    console.log("delete");
-    option.value="delete";
-    form.submit();
-  });
-
-
-  
 var _validFileExtensions = [".pdf"];    
 function ValidateSingleInput(oInput) {
     
