2df43127751554bf5db3c3ea6173ea151208e0f2
nicolas-suarez-97 <nicolas.suarez10@hotmail.com>
2019-11-07 16:33:27 -0500

    m

---
+++ lectura.js
@@ -0,0 +1,25 @@
+var file = document.getElementById('file');
+console.log(file);
+
+function loadFileAsText(){
+    console.log("aca");
+    var fileToLoad = document.getElementById("fileToLoad").files[0];
+  
+    var fileReader = new FileReader();
+    fileReader.onload = function(fileLoadedEvent){
+        var textFromFileLoaded = fileLoadedEvent.target.result;
+        var list = textFromFileLoaded.split('\n');
+        //console.log(list);
+        var result="";
+        for(var l of list){            
+            var aux = l.split('\t');            
+            if(aux[2]!=null){                
+                aux[2].replace(/\n/g, " ");
+            }
+            result += "codCiiu.set('"+aux[1]+"','"+aux[2]+"');\n";
+        }
+        document.getElementById("inputTextToSave").innerHTML = result;
+    };
+  
+    fileReader.readAsText(fileToLoad, "UTF-8");
+  }
