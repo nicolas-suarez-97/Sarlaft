3d1c7769588c0e11186bc5e2f7388c4f20fb695e
nicolas-suarez-97 <nicolas.suarez10@hotmail.com>
2019-12-03 16:06:24 -0500

    Manejo datos Natural

--- Natural/natural1.js
+++ Natural/natural1.js
@@ -4,3 +4,14 @@ var sucursal = document.getElementById('inputSucursal');
 function sucursalChange(){
     sucursal.value = ciudad.value;
 }
+
+function validateManejo(){
+    var aceptar = document.getElementById('aceptar');
+    if(aceptar.checked){
+        document.getElementById('manejoDatos').style.display = 'none';
+        document.getElementById('aceptarEnvio').checked = true;
+    }else{
+        document.getElementById('informacion').style.display = 'none';
+        document.getElementById('mensajeRechazo').style.display = 'flex';
+    }
+}
