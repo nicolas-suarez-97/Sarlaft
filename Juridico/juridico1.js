5854e8b80cc20261c0b18ccd5224e98c9c08ea8b
nicolas-suarez-97 <nicolas.suarez10@hotmail.com>
2019-12-03 16:03:25 -0500

    m

--- Juridico/juridico1.js
+++ Juridico/juridico1.js
@@ -9,6 +9,7 @@ function validateManejo(){
     var aceptar = document.getElementById('aceptar');
     if(aceptar.checked){
         document.getElementById('manejoDatos').style.display = 'none';
+        document.getElementById('aceptarEnvio').checked = true;
     }else{
         document.getElementById('informacion').style.display = 'none';
         document.getElementById('mensajeRechazo').style.display = 'flex';
